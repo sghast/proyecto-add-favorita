db03a1ee02a10ea14bb377ae547e8d9e53973d4d
Alan <alanjavierreyes318@gmail.com>
2026-07-07 22:29:25 -0500

    Implementación de comentarios

--- scripts/cleaning/clean_data.py
+++ scripts/cleaning/clean_data.py
@@ -2,31 +2,40 @@ import polars as pl
 from scripts.load.load_data import load_all_data
 
 def clean_all_data():
+    # se llaman a los datasets cargados desde load_data.py
     datasets = load_all_data()
 
+    # train
     train = datasets["train"].unique()
     train = train.with_columns(
         pl.col("date").str.to_date()
     )
 
+    # stores
     stores = datasets["stores"].unique()
 
+    # transactions
     transactions = datasets["transactions"].unique()
     transactions = transactions.with_columns(
         pl.col("date").str.to_date()
     )
 
+    # oil
     oil = datasets["oil"].unique()
     oil = oil.with_columns(
         pl.col("date").str.to_date(),
         pl.col("dcoilwtico").interpolate()
+        # interpolate completa los valores nulos o vacíos usando una estimación basada en los 
+        # valores adyacentes
     )
 
+    # holidays
     holidays = datasets["holidays"].unique()
     holidays = holidays.with_columns(
         pl.col("date").str.to_date()
     )
 
+    # se retorna un diccionario con ls datos limpios
     return {
         "train": train,
         "stores": stores,
@@ -34,6 +43,7 @@ def clean_all_data():
         "oil": oil,
         "holidays_events": holidays
     }
+
 if __name__ == "__main__":
     datasets = clean_all_data()
     for name, df in datasets.items():
